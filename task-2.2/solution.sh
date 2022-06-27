7d970c28dc3cb1bdc295b27dcfeb35e46b159e09
Kate Makovieva <94247189+Makovyova@users.noreply.github.com>
2022-06-27 21:31:43 +0300

    Update solution.sh

--- task-2.2/solution.sh
+++ task-2.2/solution.sh
@@ -1,4 +1,4 @@
 mkdir files_on_client
-scp quiz@10.31.2.1:/srv/files_on_server/* /home/quiz/files_on_client
+scp -r box@10.31.2.1:/srv/files_on_server/* /home/quiz/files_on_client
 #yes
 supersecret
